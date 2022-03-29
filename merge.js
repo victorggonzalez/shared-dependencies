061b37164bdcc9aa1d8fcf9ef7a44442d28effe1
Victor Gonzalez <victor.ggonzalez26@gmail.com>
2022-03-29 11:23:20 +0200

    added merge.js

---
+++ merge.js
@@ -0,0 +1,8 @@
+const fs = require('fs')
+
+const localPackageJson = require('../../package.json')
+const sharedPackageJson = require('./package.json')
+
+Object.assign(localPackageJson.dependencies, sharedPackageJson.dependencies)
+
+fs.writeFileSync('../../package.json', JSON.stringify(localPackageJson, null, 2))
